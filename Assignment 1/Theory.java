886a71feb136f665b6983a296495590f68aa2eca
PrathamModi001 <Pratham.mce21@sot.pdpu.ac.in>
2024-09-24 11:59:00 +0530

    first commit

---
+++ Assignment 1/Theory.java
@@ -0,0 +1,14 @@
+// Behavioral Patterns
+
+// Strategy: Defines a family of algorithms, encapsulates each one, and makes them interchangeable
+// Command: Encapsulates a request as an object, letting you parameterize clients with different requests
+
+// Creational Patterns
+
+// Abstract Factory: Provides an interface for creating families of related or dependent objects
+// Prototype: Specify the kinds of objects to create using a prototypical instance, and create new objects by copying this prototype
+
+// Structural Patterns
+
+// Decorator: Attach additional responsibilities to an object dynamically
+// Proxy: Provide a surrogate or placeholder for another object to control access to it
