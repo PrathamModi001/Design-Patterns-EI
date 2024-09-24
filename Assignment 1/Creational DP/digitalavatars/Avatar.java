09602542a04f7b9cf051c4b6037518735aa3bf67
PrathamModi001 <Pratham.mce21@sot.pdpu.ac.in>
2024-09-24 12:00:26 +0530

    start with Digital Avatar

---
+++ Assignment 1/Creational DP/digitalavatars/Avatar.java
@@ -0,0 +1,5 @@
+package digitalavatars; // Interface for Prototypes
+
+public interface Avatar extends Cloneable {
+    Avatar clone() throws CloneNotSupportedException; // Method for cloning avatars
+}
