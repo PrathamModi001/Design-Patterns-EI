5a010d8a39cd9678c9849da59bed07a6b7ee1cda
PrathamModi001 <Pratham.mce21@sot.pdpu.ac.in>
2024-09-24 12:09:16 +0530

    done

---
+++ Assignment 1/Creational DP/VR_Env/VREnvironmentExample.java
@@ -0,0 +1,13 @@
+package VR_Env;
+
+public class VREnvironmentExample {
+    public static void main(String[] args) {
+        // Generate desert world
+        VRWorldGenerator desertWorld = new VRWorldGenerator(new DesertEnvironmentFactory());
+        desertWorld.generateWorld();
+
+        // Generate ocean world
+        VRWorldGenerator oceanWorld = new VRWorldGenerator(new OceanEnvironmentFactory());
+        oceanWorld.generateWorld();
+    }
+}
