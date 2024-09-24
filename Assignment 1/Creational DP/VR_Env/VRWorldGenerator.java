bba261d6e3d9bca62a91b60bf4e8a97c36886bf9
PrathamModi001 <Pratham.mce21@sot.pdpu.ac.in>
2024-09-24 12:09:00 +0530

    world gen made

---
+++ Assignment 1/Creational DP/VR_Env/VRWorldGenerator.java
@@ -0,0 +1,19 @@
+package VR_Env;
+
+public class VRWorldGenerator {
+    private VREnvironmentFactory factory; // Environment factory reference
+
+    public VRWorldGenerator(VREnvironmentFactory factory) {
+        this.factory = factory; // Set factory during initialization
+    }
+
+    public void generateWorld() {
+        Terrain terrain = factory.createTerrain(); // Create terrain
+        Atmosphere atmosphere = factory.createAtmosphere(); // Create atmosphere
+        Inhabitants inhabitants = factory.createInhabitants(); // Create inhabitants
+
+        terrain.generate(); // Generate terrain
+        atmosphere.apply(); // Apply atmosphere
+        inhabitants.populate(); // Populate environment
+    }
+}
